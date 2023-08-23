2bacb40e9ba4c9ed6e4845ac7f5d5e7a193bdff6
933yee <kevins30102@yahoo.com.tw>
2023-08-23 22:53:01 +0800

    2023/08/23

--- cses/Nested Ranges Check.cpp
+++ cses/Nested Ranges Check.cpp
@@ -16,29 +16,39 @@ const ll M = 1e9 + 7;
 
 int n;
 vvi v;
-set<int> st;
-
-bool cmp(pii& a, pii& b) {
-    if (a.F == b.F)
-        return a.S > b.S;
-    else
-        return a.F < b.F;
-}
+vector<pii> ans;
 
 int main() {
     IOS;
     cin >> n;
-    vvi ans(2, vi(n));
+    ans.resize(n);
 
     for (int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
         v.PB({a, b, i});
     }
-    sort(v.begin(), v.end(), cmp);
-    // for (pii& i : v) {
-    //     cout << "(" << i.F << "," << i.S << ")" << '\n';
-    // }
-    for (int i = 0; i < n; i++) {
+    sort(v.begin(), v.end(), [](vi& a, vi& b) {
+        if (a[0] == b[0])
+            return a[1] > b[1];
+        else
+            return a[0] < b[0];
+    });
+    int mn = v[n - 1][1];
+    for (int i = n - 2; i >= 0; i--) {
+        if (v[i][1] >= mn)
+            ans[v[i][2]].F = 1;
+        else
+            mn = v[i][1];
     }
+    int mx = v[0][1];
+    for (int i = 1; i < n; i++) {
+        if (mx >= v[i][1])
+            ans[v[i][2]].S = 1;
+        else
+            mx = v[i][1];
+    }
+    for (int i = 0; i < n; i++) cout << ans[i].F << ' ';
+    cout << '\n';
+    for (int i = 0; i < n; i++) cout << ans[i].S << ' ';
 }
